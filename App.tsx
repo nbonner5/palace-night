980f86d03038fddad092e65e2cf0aff0b634e155
Nickolas Bonner <nbonner5@outlook.com>
2026-03-20 15:03:02 -0400

    feat(): small change to allow for background to match

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { PalaceGame } from './src/ui/PalaceGame';
+import { colors } from './src/ui/theme/colors';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider style={{ flex: 1, backgroundColor: colors.tableDark }}>
       <StatusBar style="light" />
       <PalaceGame />
     </SafeAreaProvider>
